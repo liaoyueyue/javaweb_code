5ed7974f8652c2182ca7e4cf960067603c1aacbd
liaoyueyue <973044869@qq.com>
2023-03-26 00:24:52 +0800

    help:20230326

---
+++ T20230325/src/bilibili/homework/temp.java
@@ -0,0 +1,15 @@
+package bilibili.homework;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: liaoyueyue
+ * Date: 2023-03-25
+ * Time: 23:20
+ */
+public class temp {
+    public static void main(String[] args) {
+        System.out.print("Hello world\n");
+        System.out.print("My first program");
+    }
+}
